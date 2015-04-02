26ddf445907898566dd159265fd19ffdcda50910
ComputerJunction <ivanharris@Ivans-MacBook-Pro.local>
2015-04-02 01:41:50 -0400

    Changed TableP
    Added hover capabilities but it needs to be in Php with for loop and
    numbers to make it worthwhile.

--- TableP.php
+++ TableP.php
@@ -1,57 +1,26 @@
 <?php include("inc/Header.php") ?>
+    
+    <table class="ctab tabimg">
 
-<!--    <table class="ctabtab">
-        <caption>Betting Information</caption>
- 
-        <thead>
-        <tr>
-            <th>Type of Bet</th>
-            <th>Results of Roll</th>
-            <th>Expected Value</th>
-            <th>Odds No:Yes</th>
-            <th>Payout</th>
-        </tr>
-        </thead>
-        
-        <tfoot>
-            <tr>
-                <td colspan="100%">Working towards perfection is an admirable goal, but only a goal never a realistic destination.  If you see a problem with the numbers or something that looks funky let me know            </td>        
-            </tr>
-        </tfoot>
         
         <tbody>
-        <tr>
-            <td headers="Type of Bet">Type of Bets: Place</td>
-            <td headers="Results of Roll">Number clicked: 8</td>
-            <td headers="Expected Value">Expected Value: 96cents</td>
-            <td headers="Odds">Odds: 7 : 1</td>
-            <td headers="Payout">Payout: 1.16 : 1</td>
-        </tr>
-        </tbody>
+            <tr class="frrow">
+                <td class="slidein">Pass Line</td>
+                <td class="slidein">Dont Pass</td>
+                <td class="slidein">Dont Come</td>
+                <td class="slidein">Dont Place Four</td>
+                <td class="slidein">Dont Place Five</td>
+                <td class="slidein">Dont Place Six</td>
+                <td class="slidein">Dont Place Eigth</td>
+                <td class="slidein">Dont Place Nine</td>
+                <td class="slidein">Dont Place Ten</td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+            </tr>
         
-    </table>-->
-    <table class="ctabfig tabimg">
-        <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
         </tbody>
         
     </table>
